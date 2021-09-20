aa30ec73fe79a1e3db1e6306c1bc20c49551922a
HyunTaek5 <oht222@naver.com>
2021-09-20 16:51:22 +0900

    elasticsearch module 추가

--- api/nestjs/src/controllers.module.ts
+++ api/nestjs/src/controllers.module.ts
@@ -4,13 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { AwsModule } from './aws/aws.module';
 import { AdminUserController, UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [AuthModule, UserModule, AwsModule],
-  controllers: [
-    AuthController,
-    AdminUserController,
-    UserController,
-  ],
+  imports: [AuthModule, UserModule, AwsModule, SearchModule],
+  controllers: [AuthController, AdminUserController, UserController],
 })
 export class ControllersModule {}
