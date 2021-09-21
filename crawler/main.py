d39db7a6b829982418740aecdf3325626ed54844
HyunTaek5 <oht222@naver.com>
2021-09-21 15:17:09 +0900

    Flask API 구성 & Postgres 연동 & Crawl 리턴값 변경

--- crawler/main.py
+++ crawler/main.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_restx import Api, Resource
+from crawler.crawl import find_jobs, get_jobs
+import psycopg
 
 app = Flask(__name__)
 api = Api(app)
+db = psycopg.connect(host='localhost', dbname='elastics', user='postgres', password='asdf', port='5432')
 
 
-if __name__ == "__main__":가
+@api.route('/<string:name>')
+class findJob(Resource):
+    @staticmethod
+    def get(name):
+        jobs_final = find_jobs(name) + get_jobs(name)
+        db.save(jobs_final)
+
+
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
